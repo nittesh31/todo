8eea867fcca1fc0a7daa337f1413b52cb6a7be71
nittesh31 <110118079@nitt.edu>
2021-05-29 16:56:42 +0530

    initial commit

---
+++ todo.js
@@ -0,0 +1,26 @@
+const app= new Vue ({
+    el:'#app',
+    data: {
+       tit:'NIs ToDo app',
+       newTodo:'' ,
+       todos:[]
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({
+                title:this.newTodo,
+                done:false
+            }),
+            this.newTodo=''
+        },
+        
+        removeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    allDone(){
+        this.todos.forEach(todo => { todo.done=true;})
+    }
+
+    }
+});
